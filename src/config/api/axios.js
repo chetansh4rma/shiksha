dfffbffa3314823ec504d55af0b8150de509b349
chetansh4rma <mohansharma4670@outlook.com>
2025-08-31 03:32:31 +0530

    changes

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    // "https://kollege-api.onrender.com",
-    "http://localhost:3500",
-    //? this is the nodejs port, which is 3500 by default, and not the react port(3000).
-
+    'https://shikshaserver.onrender.com',
   headers: { "Content-Type": "application/json" },
 });
